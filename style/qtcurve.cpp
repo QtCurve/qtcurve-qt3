60a2edeff3d79d0de0799c0d0d807ca7e2bde72e
craig <craig@24a705aa-185c-4198-86fd-43cf90abb1fa>
2009-05-12 19:19:43 +0000

    Corner def btn indicator should use focus colour

--- style/qtcurve.cpp
+++ style/qtcurve.cpp
@@ -2684,7 +2684,7 @@ void QtCurveStyle::drawPrimitive(PrimitiveElement pe, QPainter *p, const QRect &
                     bool         sunken(flags&Style_Down || flags&Style_Sunken);
                     int          offset(sunken ? 5 : 4),
                                  etchOffset(QTC_DO_EFFECT ? 1 : 0);
-                    const QColor *cols(itsMouseOverCols ? itsMouseOverCols : itsHighlightCols);
+                    const QColor *cols(itsFocusCols ? itsFocusCols : itsHighlightCols);
 
                     points.setPoints(3, r.x()+offset+etchOffset, r.y()+offset+etchOffset, r.x()+offset+6+etchOffset, r.y()+offset+etchOffset,
                                         r.x()+offset+etchOffset, r.y()+offset+6+etchOffset);
